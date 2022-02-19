5e3acd776b5c47b66bb2bdb145f6e38ba221c048
206 <206@academicos.cesde.edu.co>
2022-02-18 20:34:44 -0500

    Condicionales

--- main.py
+++ main.py
@@ -5,18 +5,51 @@ comentario de bloque
 
 '''
 
-#----------Variables--------------
+# ----------Variables--------------
 
 
-x = 5
+from operator import truediv
+
+
+#x = 5
 y = "John"
-print(x)
-print(y)
+# print(x)
+# print(y)
 
-x = str(3)    # x will be '3'
-y = int(3)    # y will be 3
-z = float(3)  # z will be 3.0
+# x = str(3)    # x will be '3'
+# y = int(3)    # y will be 3
+# z = float(3)  # z will be 3.0
 
 nombre = 'Carlos'
-#salida en consola
+# salida en consola
 print(nombre)
+
+# Concatenar
+print('Su nombre es : ', nombre, 'coma')
+print('Su nombre es : ' + nombre + 'mas')
+
+print(f'Su nombre es {nombre}')
+
+# variables datos primintivos
+edadUsuario = 43
+estatura = 1.84
+variableBoolean = True
+
+print(f'Su nombre es : {nombre} su edad es {edadUsuario} tiene una estatura de {estatura} mts y es incha del mejor {variableBoolean}?')
+
+# entrada de datos por consola
+#nomero1 = int(input('Ingrese el numero :'))
+#nomero2 = int(input('Ingrese el numero :'))
+
+#suma = numero1  + numero2
+#print(f'la suma es  {numero1} + {numero2} = {suma}')
+
+# condicionales logicas (IF) OJO CON LA TABULACION
+
+d = 2
+if(d > 4):
+    print('es mayor Positivo')
+else:
+    print('fallo')
+    print('ok')
+print('ok fuera del if')
