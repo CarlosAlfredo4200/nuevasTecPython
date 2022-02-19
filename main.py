3fbec58894b0d74790b9a5eee0f836eb7ccd6417
206 <206@academicos.cesde.edu.co>
2022-02-18 19:51:59 -0500

    Variables

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 # Comentar  el  python
+
+''' 
+comentario de bloque
+
+'''
+
+#----------Variables--------------
+
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+nombre = 'Carlos'
+#salida en consola
+print(nombre)
